5d385a2ab6c24754698560a65366ffba7ce2a02e
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-09-25 20:55:23 +0600

    click to details on view details button in submit donation details components

--- src/components/SubmitDonationDetails/SubmitDonationDetails.jsx
+++ src/components/SubmitDonationDetails/SubmitDonationDetails.jsx
@@ -1,5 +1,6 @@
 
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const SubmitDonationDetails = ({ donation }) => {
     const { id, picture, title, category, category_bg, card_bg, text_color, price, button_bg } = donation;
@@ -14,7 +15,7 @@ const SubmitDonationDetails = ({ donation }) => {
                         <p style={{ backgroundColor: category_bg, color: text_color }} className='py-1 px-[10px] inline rounded'>{category}</p>
                         <p className='pt-2 text-lg font-semibold text-black'>{title}</p>
                         <p style={{ color: text_color, fontWeight: '600' }}>{price}</p>
-                        <button style={{ backgroundColor: button_bg }} className="mt-1 py-2 px-4 rounded text-white text-base font-semibold">View Details</button>
+                        <Link to={`/donate/${id}`}><button style={{ backgroundColor: button_bg }} className="mt-1 py-2 px-4 rounded text-white text-base font-semibold">View Details</button></Link>
                     </div>
                 </div>
             </div>
