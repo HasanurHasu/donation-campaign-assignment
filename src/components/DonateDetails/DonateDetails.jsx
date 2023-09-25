845f08e0da7736f5cd64ae927e7546e0445b7851
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-09-25 15:02:44 +0600

    added background and title in DonateDetails

--- src/components/DonateDetails/DonateDetails.jsx
+++ src/components/DonateDetails/DonateDetails.jsx
@@ -2,20 +2,24 @@ import { useLoaderData, useParams } from "react-router-dom";
 
 const DonateDetails = () => {
     const donations = useLoaderData();
-    const {id} = useParams();
+    const { id } = useParams();
     const idInt = parseFloat(id);
 
-    
+
+
     const donation = donations.find(donation => donation.id === idInt);
-    
-console.log(donation, id);
+    const { picture, button_bg, title, description, price } = donation;
 
     return (
-        <div>
-            <h1>This is  Donation {donation.id}</h1>
-            <div>
-                <img src={donation.picture} alt="" />
+        <div className="max-w-6xl m-auto">
+            <div className="">
+                <div style={{ backgroundImage: `url(${picture})`, backgroundSize: 'cover' }} className="h-[550px] flex items-end rounded-md">
+                    <div className="bg-black opacity-80 w-full rounded-b-md"><button style={{ backgroundColor: button_bg }} className="m-7 py-4 px-6 rounded text-white text-lg font-semibold">Donate {price}</button></div>
+                </div>
             </div>
+
+            <h1 className="text-[#0B0B0B] text-4xl font-bold my-7">{title}</h1>
+            <p className="text-base text-justify">{description}</p>
         </div>
     );
 };
