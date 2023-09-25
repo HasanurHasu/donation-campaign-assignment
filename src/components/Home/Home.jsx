e1176d15dcfaa9b09a2940c9e0bffa7cc6ae9d4d
Md. Hasanur Rahman <hasanurhasu121@gmail.com>
2023-09-25 10:09:29 +0600

    update donation.json file

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
         <div className="max-w-6xl m-auto">
             <h1 className="text-center">This is home page {donations.length}</h1>
-            <div className="grid grid-cols-4 gap-6">
+            <div className="md:grid lg:grid-cols-4 md:grid-cols-2 gap-6">
                 {
                     donations.map((donation, idx) => <Card key={idx} donation={donation}></Card>)
                 }
