c160b7215d790d336d1533ae87839620194a81b5
Md. Hasanur Rahman <hasanurhasu121@gmail.com>
2023-09-27 00:10:08 +0600

    added pie chart headline with color

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { PieChart, Pie, Cell, } from 'recharts';
+import { PieChart, Pie, Cell, Tooltip, Legend, } from 'recharts';
 import { getStoredDonations } from '../utility/localStorage';
 
 const Statistics = () => {
@@ -13,12 +13,12 @@ const Statistics = () => {
     if (donations.length > 0) {
       const donationSubmit = donations.filter(donation => storedDonationId.includes(donation.id));
       setSubmitDonation(donationSubmit);
-      
+
     }
   }, [donations])
   const data = [
-    { name: 'Your Donation', value: submitDonation.length },
-    { name: 'Total Donation', value: donations.length }
+    { name: 'Total Donation', value: donations.length },
+    { name: 'Your Donation', value: submitDonation.length }
   ];
 
   const COLORS = ['#FF444A', '#00C49F'];
@@ -31,13 +31,13 @@ const Statistics = () => {
 
     return (
       <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-        {`${(percent * 100).toFixed(0)}%`}
+        {`${(percent * 100).toFixed(1)}%`}
       </text>
     );
   };
 
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center flex-col items-center'>
       <PieChart width={400} height={400}>
         <Pie
           data={data}
@@ -49,11 +49,21 @@ const Statistics = () => {
           fill="#8884d8"
           dataKey="value"
         >
+          <Legend verticalAlign="top" height={36} />
+
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
         </Pie>
       </PieChart>
+      <div className='flex gap-14'>
+        <div className='flex justify-center items-center gap-3'>
+          <h1 className='text-lg text-[#0B0B0B]'>{data[1].name}</h1><p className='bg-[#00C49F] h-3 w-24 rounded-sm'>&apos;</p>
+        </div>
+        <div className='flex justify-center items-center gap-3'>
+          <h1 className='text-lg text-[#0B0B0B]'>{data[0].name}</h1><p className='bg-[#FF444A] h-3 w-24 rounded-sm'>&apos;</p>
+        </div>
+      </div>
     </div>
   );
 };
