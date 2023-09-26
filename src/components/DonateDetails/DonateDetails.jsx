ffa6154e43bba0a8e3a11fc2261086fcf81896c4
Md. Hasanur Rahman <hasanurhasu121@gmail.com>
2023-09-27 01:43:41 +0600

    responsive donate Details

--- src/components/DonateDetails/DonateDetails.jsx
+++ src/components/DonateDetails/DonateDetails.jsx
@@ -18,14 +18,14 @@ const DonateDetails = () => {
     const { picture, button_bg, title, description, price } = donation;
 
     return (
-        <div className="max-w-6xl m-auto">
+        <div className="max-w-6xl lg:m-auto mx-8 mt-10 lg:mt-10">
             <div className="">
-                <div style={{ backgroundImage: `url(${picture})`, backgroundSize: 'cover' }} className="h-[550px] flex items-end rounded-md">
-                    <div className="bg-black opacity-80 w-full rounded-b-md"><button onClick={handleDonate} style={{ backgroundColor: button_bg }} className="m-7 py-4 px-6 rounded text-white text-lg font-semibold">Donate {price}</button></div>
+                <div style={{ backgroundImage: `url(${picture})`, backgroundSize: 'cover' }} className="h-56 md:h-[300px] lg:h-[550px] flex items-end rounded-md">
+                    <div className="bg-black opacity-80 w-full rounded-b-md"><button onClick={handleDonate} style={{ backgroundColor: button_bg }} className="m-4 lg:m-7 py-2 lg:py-4 px-3 lg:px-6 rounded text-white text-base lg:text-lg font-semibold">Donate {price}</button></div>
                 </div>
             </div>
 
-            <h1 className="text-[#0B0B0B] text-4xl font-bold my-7">{title}</h1>
+            <h1 className="text-[#0B0B0B] text-xl md:text-3xl lg:text-4xl font-bold my-7">{title}</h1>
             <p className="text-base text-justify">{description}</p>
             <ToastContainer
                 position="top-center"
